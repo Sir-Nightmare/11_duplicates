c4fa0a8759e89ba43ee23c64dc729869022948ef
Sir Nightmare <ivantochilkin@gmail.com>
2016-10-09 18:37:33 +0300

    the skript was done

--- duplicates.py
+++ duplicates.py
@@ -1,12 +1,11 @@
 import os
-
+from tabulate import tabulate
 import sys
-import pytz
 
 
-def are_files_duplicates(folder_path):
+def find_files_duplicates(folder_path):
     set_of_files = set()
-    list_of_dublicated_files = []
+    list_of_duplicated_files = []
     for root_folder, sub_folders, file_names in os.walk(folder_path):
         for file_name in file_names:
             file_full_path = os.path.join(root_folder, file_name)
@@ -15,11 +14,29 @@ def are_files_duplicates(folder_path):
             if name_size_of_the_file not in set_of_files:
                 set_of_files.add(name_size_of_the_file)
             else:
-                list_of_dublicated_files.append([file_name, file_size, file_full_path])
-    return list_of_dublicated_files
+                list_of_duplicated_files.append([file_name, file_size, file_full_path])
+    return list_of_duplicated_files
+
+
+def print_list_of_duplicates(list_of_duplicated_files):
+    print('The list of duplicated files:')
+    print(tabulate(list_of_duplicated_files,
+                   headers=['File name', 'Size in bytes', 'Full path to the file'], tablefmt="orgtbl"))
+
+
+def delete_files(list_to_delete):
+    for file in list_to_delete:
+        os.unlink(file[2])
+    print('All duplicated files were permanently deleted.')
 
 
 if __name__ == '__main__':
-    # folder_path=sys.argv[1]
-    folder_path = 'D:\groot_folder'
-    are_files_duplicates(folder_path)
+    folder_path = sys.argv[1]
+    list_of_duplicated_files = find_files_duplicates(folder_path)
+    if len(list_of_duplicated_files) > 0:
+        print_list_of_duplicates(list_of_duplicated_files)
+        are_to_delete = input('\nDo you want do delete all duplicates permanently? (y/n)\n')
+        if are_to_delete == 'y':
+            delete_files(list_of_duplicated_files)
+    else:
+        print('There is no duplicated files.')
